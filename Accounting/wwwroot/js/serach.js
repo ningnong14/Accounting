df41ff91222e50daee89aa02e2e3353126e67e9d
Boss <nithipat.boss@gmail.com>
2022-12-09 14:47:29 +0700

    Auto stash before merge of "nithipat_PC" and "origin/nithipat_PC"

---
+++ Accounting/wwwroot/js/serach.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(() => {
+    $('#buttonSearch').click(function () {
+        var serachResult = {
+            NameCompany: $('#nameCompany').val(),
+            TagId: $('#tagId').val(),
+            Date: $('#date').val(),
+            Description: $('#description').val()
+        };
+        console.log(serachResult);
+    })
+    $('#buttonClear').click(function () {
+        $('#nameCompany').val("1");
+        $('#tagId').val("");
+        $('#date').val("");
+        $('#description').val("");
+    })
+});
