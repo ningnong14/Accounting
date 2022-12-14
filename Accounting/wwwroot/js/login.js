0d83ebdd4de61d99a9db61f96aef443ad4687622
Boss <nithipat.boss@gmail.com>
2022-12-14 12:02:14 +0700

    แก้ไขหน้า Login เปลี่ยนมาใช้ sweetalert แสดงผล

--- Accounting/wwwroot/js/login.js
+++ Accounting/wwwroot/js/login.js
@@ -1,11 +1,21 @@
 ﻿$(document).ready(() => {
-    $('#buttonSummit').click(function () {
+    let login = "";
+    let password = "";
+
+    Swal.fire({
+        title: 'Login Form',
+        html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
+               <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+        confirmButtonText: 'Sign in',
+        focusConfirm: false,
+        preConfirm: () => {
+            login = Swal.getPopup().querySelector('#login').value
+            password = Swal.getPopup().querySelector('#password').value
+        }
+    }).then((result) => {
         url = baseURL() + `api/Login/ValidateUser`;
-        username = $('#textUserName').val();
-        password = $('#textPassword').val();
-        console.log(url);
-        console.log(username);
-        console.log(password);
+        username = login;
+        password = password;
         fetch(url, {
             method: "POST",
             headers: [
@@ -17,16 +27,31 @@
                 username: username,
                 password: password
             }),
-            })
+        })
             .then(function (response) {
-                console.log(response);
-                if (response.status == "200") {
-                    
+                if (response.ok == false) {
+                    /*loading('show');*/
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Login fail',
+                        text: 'Username Password Wrong!',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'ตกลง'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = baseURL() + "/login";
+                        }
+                    })
                 }
                 return response.json() // แปลงข้อมูลที่ได้เป็น json
             })
             .then(function (data) {
-                console.log(data); // แสดงข้อมูล JSON จาก then ข้างบน
+                if (data.code == "200") {
+                    localStorage.setItem("Username", data.username);
+                    console.log(baseURL());
+                    window.location.href = baseURL() + "/Serach";
+                }
+                console.log("LoginData", data); // แสดงข้อมูล JSON จาก then ข้างบน
             })
     })
-})
+});
