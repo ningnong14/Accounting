38bda7fcb8df31db6a470529727e74c2a89c4e29
Boss <nithipat.boss@gmail.com>
2022-12-27 16:57:29 +0700

    หน้า add main acc

---
+++ Accounting/Controllers/AddMainAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+    public class AddMainAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Accounting/Controllers/ApiControllers/AddMainAccountController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers.ApiControllers
+{
+    public class AddMainAccountController : Controller
+    {
+    }
+}
