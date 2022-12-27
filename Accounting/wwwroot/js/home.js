fd1e2941ce90cc09e21c848cf10de2cd1a6d873e
Boss <nithipat.boss@gmail.com>
2022-12-27 16:57:48 +0700

    แก้ไข function home และ ลิ้งหน้า

--- Accounting/wwwroot/js/home.js
+++ Accounting/wwwroot/js/home.js
@@ -1,3 +1,12 @@
 ﻿$(document).ready(() => {
-
+    console.log("already function");
+    $('#function_1').on("click", function (event) {
+        window.location.href = baseURL() + "/serach";
+    });
+    $('#function_2').on("click", function (event) {
+        window.location.href = baseURL() + "/AddMainAccount";
+    });
+    $('#function_3').on("click", function (event) {
+        window.location.href = baseURL() + "/RecordAccounting";
+    });
 });
